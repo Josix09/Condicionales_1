6c425aa9d26bbb4443711cf1e04fed8d7af25720
Josix09 <jorgesilva@colegioguanenta.edu.co>
2025-01-31 09:17:16 -0500

    se agregaron y actualizaron archivos

--- condicionales_1.py
+++ condicionales_1.py
@@ -1,18 +1,14 @@
 # input
-
-cant_minutos=input("dijite la cant_minutos: ")
-
-cant_minutos=int(cant_minutos)
+cant_minutos=int(input("dijite la cant_minutos: "))
 
 # processing
-
 if cant_minutos<=3:
-    valor_llamada=300
+    print("el valor de la llamada es : 300 pesos")
     
 
 else:
     valor_llamada=300+50*(cant_minutos-3)
 
 #output
-print("el valor de la llamada es: "+str(valor_llamada))
+print ("el valor de la llamada es: "+str(valor_llamada))
 
